754b342553eb196f20febad3849106605e38ed5a
nuelscode <chinagorom.onwubiko@gmail.com>
2020-03-03 08:07:48 -0800

    modified footer

--- src/main/Footer.jsx
+++ src/main/Footer.jsx
@@ -3,12 +3,14 @@ import React from "react";
 const Footer = () => {
   return (
     <div>
-      <footer className="w-full text-center border-t border-grey p-4 pin-b">
-               <p className="flex justify-start">contact us</p>
+      <footer className="w-full text-center border-t border-grey pin-b">
+               <p className="flex justify-start pl-5 ">contact us</p>
             <div className="flex justify-start bg-blue-400 text-white h-100px mt-100px">
-            <i className="fa fa-twitter"> follow us on twitter</i>
+            <i className="fa fa-twitter pl-3"> follow us on twitter</i>
             </div>
-            <p className="bg-gray-400">2020 all rights reserved</p>
+            <div className="bg-blue-400 text-white h-100px">
+            <i className="fa fa-copyright"> 2020 all rights reserved</i>
+            </div>
       </footer>
     </div>
   );
