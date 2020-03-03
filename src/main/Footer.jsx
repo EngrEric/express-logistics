7e356d70e563c68c124c8db046b99fa95a1bfc89
nuelscode <chinagorom.onwubiko@gmail.com>
2020-03-03 08:18:39 -0800

    tweaked padding

--- src/main/Footer.jsx
+++ src/main/Footer.jsx
@@ -4,9 +4,9 @@ const Footer = () => {
   return (
     <div>
       <footer className="w-full text-center border-t border-grey pin-b">
-               <p className="flex justify-start pl-5 ">contact us</p>
+               <p className="flex justify-start pl-3 ">contact us</p>
             <div className="flex justify-start bg-blue-400 text-white h-100px mt-100px">
-            <i className="fa fa-twitter pl-3"> follow us on twitter</i>
+            <i className="fa fa-twitter pl-2"> follow us on twitter</i>
             </div>
             <div className="bg-blue-400 text-white h-100px">
             <i className="fa fa-copyright"> 2020 all rights reserved</i>
